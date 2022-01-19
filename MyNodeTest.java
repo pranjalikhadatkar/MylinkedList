ea5d9a60537d3e5472ac78f11bf3145fe58657c4
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-19 22:11:07 +0530

    Added: given 3 numbers added to top in linklist

--- MyNodeTest.java
+++ MyNodeTest.java
@@ -6,19 +6,22 @@ import org.junit.Test;
 
 public class MyNodeTest {
 	 @Test
-	  public void given3NumbersWhenLinkedShouldPassedLinkedListTest()
-	 {
-		 
-			MyNode<Integer> myFirstNode = new MyNode<>(56);
+	 public void given3NumbersWhenAddedToLinkedlistShouldBeAddedToTop(){
+	        MyNode<Integer> myFirstNode = new MyNode<>(70);
 	        MyNode<Integer> mySecondNode = new MyNode<>(30);
-	        MyNode<Integer> myThirdNode = new MyNode<>(70);
-	       
-	        myFirstNode.setNext(mySecondNode);
-	       mySecondNode.setNext(myThirdNode);
-	        // check if linked list created
-	        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
-	                         mySecondNode.getNext().equals(myThirdNode);
-	        //System.out.println(result);
+	        MyNode<Integer> myThirdNode = new MyNode<>(56);
+	        
+	        MyLinkedList<Integer> MyLinkedlist=new MyLinkedList<Integer>();
+	        MyLinkedlist.add(myFirstNode);
+	        MyLinkedlist.add(mySecondNode);
+	        MyLinkedlist.add(myThirdNode);
+	        MyLinkedlist.printMyNodes();
+	        
+	        boolean result= MyLinkedlist.head.getNext().equals(myThirdNode) &&
+	        		MyLinkedlist.head.getNext().equals(mySecondNode) &&
+	        		MyLinkedlist.head.getNext().equals(myFirstNode);
 	        Assert.assertTrue(result);
-      }
-}
+	        
+	 } 
+
+	}

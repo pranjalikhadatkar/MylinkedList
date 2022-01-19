7dff7c7f2bf2e3ef2413a153340bf8d9d6f26081
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-19 21:41:39 +0530

    Added: test cases of linklist project file

---
+++ MyLinkedList.java
@@ -0,0 +1,71 @@
+package LinkListPractice;
+
+public class MyLinkedList<K> {
+	public INode<K> head;
+	public INode<K> tail;
+	
+	public MyLinkedList() {
+		this.head=null;
+		this.tail=null;
+	}
+	
+	public void add(INode<K> newNode)
+	{
+		if(this.tail == null)
+		{
+			this.tail = newNode;
+		}
+		if(this.head == null)
+		{
+			this.head = newNode;
+		}
+		else
+		{
+			INode<K> tempNode = this.head;
+			this.head= newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+	
+	public void append(INode<K> MyNode)
+	{
+		if(this.head == null) {
+			this.head = MyNode;
+		}
+		if(this.tail == null) {
+			this.tail = MyNode;
+		}else {
+			this.tail.setNext(MyNode);
+			this.tail = MyNode;
+		}
+	}
+	
+	public void insert(INode<K> myNode, INode<K> newNode)
+	{
+		INode<K> tempNode = myNode.getNext();
+		myNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
+	
+	public INode<K> pop(){
+		INode<K> tempNode = this.head;
+		this.head = head.getNext();
+		return tempNode;	
+	}
+	
+	public INode<K> popLast() {
+		INode<K> tempNode = head;
+		while(!tempNode.getNext().equals(tail));
+		{
+			tempNode = tempNode.getNext();
+			}
+		this.tail = tempNode;
+		tempNode = tempNode.getNext();
+		return tempNode;
+	}
+	
+	public void printMyNodes()
+	{
+		System.out.println("My Nodes: "+head);
+	}	
+}
---
+++ MyNodeTest.java
@@ -0,0 +1,24 @@
+package LinkListPractice;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class MyNodeTest {
+	 @Test
+	  public void given3NumbersWhenLinkedShouldPassedLinkedListTest()
+	 {
+		 
+			MyNode<Integer> myFirstNode = new MyNode<>(56);
+	        MyNode<Integer> mySecondNode = new MyNode<>(30);
+	        MyNode<Integer> myThirdNode = new MyNode<>(70);
+	       
+	        myFirstNode.setNext(mySecondNode);
+	       mySecondNode.setNext(myThirdNode);
+	        // check if linked list created
+	        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+	                         mySecondNode.getNext().equals(myThirdNode);
+	        //System.out.println(result);
+	        Assert.assertTrue(result);
+      }
+}

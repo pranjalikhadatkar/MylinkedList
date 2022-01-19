b595fefdc06ce01a79b6885c687dfc9d1246cec0
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-19 22:21:56 +0530

    Added: given 3 numbers append in linklist

--- MyNodeTest.java
+++ MyNodeTest.java
@@ -6,22 +6,22 @@ import org.junit.Test;
 
 public class MyNodeTest {
 	 @Test
-	 public void given3NumbersWhenAddedToLinkedlistShouldBeAddedToTop(){
-	        MyNode<Integer> myFirstNode = new MyNode<>(70);
+	 public void given3NumbersWhenAppendShouldBeAddedToLast(){
+	        MyNode<Integer> myFirstNode = new MyNode<>(56);
 	        MyNode<Integer> mySecondNode = new MyNode<>(30);
-	        MyNode<Integer> myThirdNode = new MyNode<>(56);
+	        MyNode<Integer> myThirdNode = new MyNode<>(70);
 	        
 	        MyLinkedList<Integer> MyLinkedlist=new MyLinkedList<Integer>();
-	        MyLinkedlist.add(myFirstNode);
-	        MyLinkedlist.add(mySecondNode);
-	        MyLinkedlist.add(myThirdNode);
+	        MyLinkedlist.append(myFirstNode);
+	        MyLinkedlist.append(mySecondNode);
+	        MyLinkedlist.append(myThirdNode);
 	        MyLinkedlist.printMyNodes();
 	        
 	        boolean result= MyLinkedlist.head.getNext().equals(myThirdNode) &&
 	        		MyLinkedlist.head.getNext().equals(mySecondNode) &&
 	        		MyLinkedlist.head.getNext().equals(myFirstNode);
-	        Assert.assertTrue(result);
-	        
+	        Assert.assertTrue(result);      
 	 } 
-
 	}
+
+	

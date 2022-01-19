5b04a17498aace8e83fb712893a3a5a7b6b45ee5
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-19 22:35:11 +0530

    Added: given 3 numbers popatfirst linklist

--- MyNodeTest.java
+++ MyNodeTest.java
@@ -6,21 +6,24 @@ import org.junit.Test;
 
 public class MyNodeTest {
 	 @Test
-	 public void given3NumbersWhenInsertingSecondInBetweenShouldPassedLinkedlistResult(){
+	 public void givenLastElementWhenDeletedShouldPassedLinkedlistResult(){
 	        MyNode<Integer> myFirstNode = new MyNode<>(56);
 	        MyNode<Integer> mySecondNode = new MyNode<>(30);
 	        MyNode<Integer> myThirdNode = new MyNode<>(70);
 	        
 	        MyLinkedList<Integer> MyLinkedlist=new MyLinkedList<Integer>();
 	        MyLinkedlist.add(myFirstNode);
+	        MyLinkedlist.append(mySecondNode);
 	        MyLinkedlist.append(myThirdNode);
-	        MyLinkedlist.insert(myFirstNode, mySecondNode);
+	        System.out.print("Initial Linked List: ");
 	        MyLinkedlist.printMyNodes();
 	        
-	        boolean result= MyLinkedlist.head.getNext().equals(myThirdNode) &&
-	        		MyLinkedlist.head.getNext().equals(mySecondNode) &&
-	        		MyLinkedlist.head.getNext().equals(myFirstNode);
-	        Assert.assertTrue(result);      
-	 } 
-	}
-	
+	        MyLinkedlist.pop();
+	        System.out.print("Linked List after popping first element: ");
+	        MyLinkedlist.printMyNodes();
+	        
+	        boolean result= MyLinkedlist.equals(mySecondNode) &&
+	        		MyLinkedlist.equals(myThirdNode); 
+	        Assert.assertTrue(result);    
+	     }
+	   }
